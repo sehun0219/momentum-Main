47371ea9adbf3c17e01af742e3eb32738da5cdce
Sehun Kim <cloudnine0219@gmail.com>
2023-04-28 16:36:45 +1200

    greetings.js

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-const username = loginInput.value;
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  const nameThatUserWorte = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, nameThatUserWorte);
+  paintGreetings(nameThatUserWorte);
 }
-
-function paintGreetings() {
-  const username = loginInput.value;
-  greeting.innerText = `Hello ${username}`;
+function paintGreetings(username) {
+  greeting.innerText = `Hello! ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
